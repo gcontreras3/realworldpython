51dc5f1390ddbecf43ac6f63d8d5e033068b807d
gcontreras3 <gcontreras31996@gmail.com>
2026-01-27 13:22:35 -0600

    finished BayesRule.py

--- BayesRule.py
+++ BayesRule.py
@@ -42,39 +42,263 @@ class Search():
         self.sep2 = 0
         self.sep3 = 0
 
-def draw_map(self, last_known):
-    """Display basemap with scale, last known [x, y] location, search areas."""
-    cv.line(self.img, (20, 270), (70, 370), (0, 0, 0), 2) # Scale bar using line() method pass base map image, a tuple of the left & right coordinates, a tuple of the color in BGR format, and the thickness of the line.
-    cv.putText(self.img, '0', (8, 370), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 0)) # putText() method to annotate the scale bar, passing the attriute for the base map image, the text to display, a tuple for the bottom-left corner of the text, the font type, font scale, and color in BGR format.
-    cv.putText(self.img, '50 Nautical Miles', (71, 370), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 0)) # Annotate the scale bar with text, passing the attribute for the base map image, the text to display, a tuple for the upper-left corner of the text, the font type, font scale, and color in BGR format.
+    def draw_map(self, last_known):
+        """Display basemap with scale, last known [x, y] location, search areas."""
+        cv.line(self.img, (20, 370), (70, 370), (0, 0, 0), 2) # Scale bar using line() method pass base map image, a tuple of the left & right coordinates, a tuple of the color in BGR format, and the thickness of the line.
+        cv.putText(self.img, '0', (8, 370), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 0)) # putText() method to annotate the scale bar, passing the attriute for the base map image, the text to display, a tuple for the bottom-left corner of the text, the font type, font scale, and color in BGR format.
+        cv.putText(self.img, '50 Nautical Miles', (71, 370), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 0)) # Annotate the scale bar with text, passing the attribute for the base map image, the text to display, a tuple for the upper-left corner of the text, the font type, font scale, and color in BGR format.
 
-    cv.rectangle(self.img, (SA1_CORNERS[0], SA1_CORNERS[1]), (SA1_CORNERS[2], SA1_CORNERS[3]), (0, 0, 0), 1)
-    cv.putText(self.img, '1', (SA1_CORNERS[0] + 3, SA1_CORNERS[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, 0)
+        cv.rectangle(self.img, (SA1_CORNERS[0], SA1_CORNERS[1]), (SA1_CORNERS[2], SA1_CORNERS[3]), (0, 0, 0), 1)
+        cv.putText(self.img, '1', (SA1_CORNERS[0] + 3, SA1_CORNERS[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, 0)
 
-    cv.rectangle(self.img, (SA2_CORNERS[0], SA2_CORNERS[1]), (SA2_CORNERS[2],  SA2_CORNERS[3]), (0, 0 , 0), 1)
-    cv.putText(self.img, '2', (SA2_CORNERS[0] + 3, SA2_CORNERS[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, 0)
+        cv.rectangle(self.img, (SA2_CORNERS[0], SA2_CORNERS[1]), (SA2_CORNERS[2],  SA2_CORNERS[3]), (0, 0 , 0), 1)
+        cv.putText(self.img, '2', (SA2_CORNERS[0] + 3, SA2_CORNERS[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, 0)
 
-    cv.rectangle(self.img, (SA3_CORNERS[0], SA3_CORNERS[1]), (SA3_CORNERS[2], SA3_CORNERS[3]), (0, 0, 0), 1)
-    cv.putText(self.img, '3', (SA3_CORNERS[0] + 3, SA3_CORNERS[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, 0)
+        cv.rectangle(self.img, (SA3_CORNERS[0], SA3_CORNERS[1]), (SA3_CORNERS[2], SA3_CORNERS[3]), (0, 0, 0), 1)
+        cv.putText(self.img, '3', (SA3_CORNERS[0] + 3, SA3_CORNERS[1] + 15), cv.FONT_HERSHEY_PLAIN, 1, 0)
 
-    cv.putText(self.img, '+', (last_known), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
-    cv.putText(self.img, '+ = Last Known Position', (274, 355), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
-    cv.putText(self.img, '* = Actual Position', (275, 370), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 0))
+        # Post the last known location of the sailor.
+        cv.putText(self.img, '+', (last_known), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
+        cv.putText(self.img, '+ = Last Known Position', (274, 355), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
+        cv.putText(self.img, '* = Actual Position', (275, 370), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 0))
+        
+        cv.imshow('Search Area', self.img)
+        cv.moveWindow('Search Area', 750, 10)
+        cv.waitKey(500)
+
+    def sailor_final_location(self, num_search_areas):
+        """Return the actual x, y location of the missing sailor."""
+        # Find sailor coordinates with respect to any SEarch Area subarray.
+        self.sailor_actual[0] = np.random.choice(self.sa1.shape[1])
+        self.sailor_actual[1] = np.random.choice(self.sa1.shape[0])
+
+        # Pick a search area at random.
+        area = int(random.triangular(1, num_search_areas + 1))
+
+        # Convert local search area coordinates to map coordinates.
+        if area == 1:
+            x = self.sailor_actual[0] + SA1_CORNERS[0]
+            y = self.sailor_actual[1] + SA1_CORNERS[1]
+            self.area_actual = 1
+        elif area == 2:
+            x = self.sailor_actual[0] + SA2_CORNERS[0]
+            y = self.sailor_actual[1] + SA2_CORNERS[1]
+            self.area_actual = 2
+        elif area == 3:
+            x = self.sailor_actual[0] + SA3_CORNERS[0]
+            y = self.sailor_actual[1] + SA3_CORNERS[1]
+            self.area_actual = 3
+        return x, y
+
+    def calc_search_effectivesness(self):
+        """SEt decimal search effectivesness value per search area."""
+        self.sep1 = random.uniform(0.2, 0.9)
+        self.sep2 = random.uniform(0.2, 0.9)
+        self.sep3 = random.uniform(0.2, 0.9)
+
+    def conduct_search(self, area_num, area_array, effectivesness_prob):
+        """Return search results and list of searched coordinates."""
+        local_y_range = range(area_array.shape[0])
+        local_x_range = range(area_array.shape[1])
+        coords = list(itertools.product(local_x_range, local_y_range))
+        random.shuffle(coords)
+        coords = coords[:int((len(coords) * effectivesness_prob))]
+        loc_actual = (self.sailor_actual[0], self.sailor_actual[1])
+        if area_num == self.area_actual and loc_actual in coords:
+            return 'Found in Area {}.'.format(area_num), coords
+        else:
+            return 'Not Found', coords
     
-    cv.imshow('Search Area', self.img)
-    cv.moveWindow('Search Area', 750, 10)
-    cv.waitKey(500)
+    def revise_target_probs(self):
+        """Rupdate area target probabilities based on search effectiveness."""
+        denom = self.p1 * (1 - self.sep1) + self.p2 * (1 - self.sep2) + self.p3 * (1 - self.sep3)
+        self.p1 = self.p1 * (1 - self.sep1) / denom
+        self.p2 = self.p2 * (1 - self.sep2) / denom
+        self.p3 = self.p3 * (1 - self.sep3) / denom
 
-# def bayes_rule(prior, likelihood, evidence):
-#     """
-#     Calculate the posterior probability using Bayes' Rule.
+def draw_menu(search_num):
+    """Print menu of choices for conducting area searches."""
+    print('\nSearch {}'.format(search_num))
+    print(
+        """
+        Choose next areas to search:
+        1. Search Area 1
+        2. Search Area 2
+        3. Search Area 3
+        4. Search Areas 1 & 2
+        5. Search Areas 1 & 3
+        6. Search Areas 2 & 3
+        7. Start Over
+        """
+        )
+    
+def main():
+    app = Search('Cape_python')
+    app.draw_map(last_known=(160, 290))
+    sailor_x, sailor_y = app.sailor_final_location(num_search_areas=3)
+    print("-" * 65)
+    print("\nInitial Target (P) Probabilitites:")
+    print("P1 = {:.3f}, P2 = {:.3f}, P3 = {:.3f}".format(app.p1, app.p2, app.p3))
+    search_num = 1
 
-#     Parameters:
-#     prior (float): Prior probability of the hypothesis.
-#     likelihood (float): Likelihood of the evidence given the hypothesis.
-#     evidence (float): Total probability of the evidence.
+    while True:
+        app.calc_search_effectivesness()
+        draw_menu(search_num)
+        choice = input("Choice: ")
 
-#     Returns:
-#     float: Posterior probability of the hypothesis given the evidence.
-#     """
-#     return (likelihood * prior) / evidence
+        if choice == "0":
+            sys.exit()
+
+        elif choice == "1":
+            results_1, coords_1 = app.conduct_search(1, app.sa1, app.sep1)
+            results_2, coords_2 = app.conduct_search(1, app.sa1, app.sep1)
+            app.sep1 = (len(set(coords_1 + coords_2))) / (len(app.sa1)**2)
+            app.sep2 = 0
+            app.sep3 = 0
+            
+        elif choice == "2":
+            results_1, coords_1 = app.conduct_search(2, app.sa2, app.sep2)
+            results_2, coords_2 = app.conduct_search(2, app.sa2, app.sep2)
+            app.sep1 = 0
+            app.sep2 = (len(set(coords_1 + coords_2))) / (len(app.sa2)**2)
+            app.sep3 = 0
+        
+        elif choice == "3":
+            results_1, coords_1 = app.conduct_search(3, app.sa3, app.sep3)
+            results_2, coords_2 = app.conduct_search(3, app.sa3, app.sep3)
+            app.sep1 = 0
+            app.sep2 = 0
+            app.sep3 = (len(set(coords_1 + coords_2))) / (len(app.sa3)**2)
+
+        elif choice == "4":
+            results_1, coords_1 = app.conduct_search(1, app.sa1, app.sep1)
+            results_2, coords_2 = app.conduct_search(2, app.sa2, app.sep2)
+            app.sep3 = 0
+
+        elif choice == "5":
+            results_1, coords_1 = app.conduct_search(1, app.sa1, app.sep1)
+            results_2, coords_2 = app.conduct_search(3, app.sa3, app.sep3)
+            app.sep2 = 0
+
+        elif choice == "6":
+            results_1, coords_1 = app.conduct_search(2, app.sa2, app.sep2)
+            results_2, coords_2 = app.conduct_search(3, app.sa3, app.sep3)
+            app.sep1 = 0
+
+        elif choice == "7":
+            main()
+
+        else:
+            print("\nSorry, but that isn't a valid choice.", file=sys.stderr)
+            continue
+
+        app.revise_target_probs() # Use Bayes' Rule to revise target probabilities.
+
+        print("\nSearch {} Results 1 = {}".format(search_num, results_1), file=sys.stderr)
+        print("Search {} Results 2 = {}\n".format(search_num, results_2), file=sys.stderr)
+        print("Search {} Effectivesness (E):".format(search_num))
+        print("E1 = {:.3f}, E2 = {:.3f}, E3 = {:.3f}".format(app.sep1, app.sep2, app.sep3))
+
+        if results_1 =='Not Found' and results_2 == 'Not Found':
+            print("\nNew Target Probabilities (P) for Search {}:".format(search_num + 1))
+            print("P1 = {:.3f}, P2 = {:.3f}, P3 = {:.3f}".format(app.p1, app.p2, app.p3))
+            
+        else:
+            cv.circle(app.img, (sailor_x, sailor_y), 3, (255, 0, 0), -1)
+            cv.imshow('Search Area', app.img)
+            cv.waitKey(1500)
+            main()
+        search_num += 1
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def bayes_rule(prior, likelihood, evidence):
+    #     """
+    #     Calculate the posterior probability using Bayes' Rule.
+
+    #     Parameters:
+    #     prior (float): Prior probability of the hypothesis.
+    #     likelihood (float): Likelihood of the evidence given the hypothesis.
+    #     evidence (float): Total probability of the evidence.
+
+    #     Returns:
+    #     float: Posterior probability of the hypothesis given the evidence.
+    #     """
+    #     return (likelihood * prior) / evidence
